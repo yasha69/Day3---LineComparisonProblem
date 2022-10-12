c04202f4149c2e76e34ca49f9f16a8ac6460a698
Yash <yashatkulwar2000@gmail.com>
2022-10-12 20:51:29 +0530

    added Welcome Message

---
+++ LineComparison.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.LineComparison;
+
+public class LineComparison {
+    public static void main(String args []) {
+        System.out.println("WELCOME TO LINE COMPARISON COMPUTATION PROGRAM ");
+    }
+}
